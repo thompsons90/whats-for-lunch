6c491ecf011f1f8163bb5a9be4fb4de50e52551e
Scott E Thompson <sthompson082290@gmail.com>
2023-06-09 16:00:45 -0500

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   base: "/whats-for-lunch/",
 });
 
+// testing base deploy
+
 // this define removes the inline testing code from production build
 // export default defineConfig({
 //   define: {

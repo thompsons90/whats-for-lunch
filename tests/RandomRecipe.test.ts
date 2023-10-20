bde6320d4a305cdf06fa9218c3344c5bf2f439da
thompsons90 <sthompson082290@gmail.com>
2023-10-20 17:19:33 -0500

    Updated vite config and random recipe test

--- tests/RandomRecipe.test.ts
+++ tests/RandomRecipe.test.ts
@@ -1,13 +1,14 @@
 
 import { test, expect } from '@playwright/test';
 import axios from 'axios';
+import { apiKey }from '../src/assets/myUrls';
 
 test('RecipeComponent fetches data from API correctly', async ({ page }) => {
   // Launch the browser and open the test page
-  await page.goto('http://localhost:3000'); // Replace with your local server URL
+  await page.goto('http://localhost:5173'); // Replace with your local server URL
 
   // Check if the API call is made correctly
-  const response = await axios.get('https://api.spoonacular.com/recipes/random?apiKey=YOUR_API_KEY'); // Replace with your actual API key
+  const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`); // Replace with your actual API key
 
   // Check if the data fetched is not empty
   const recipeData = response.data.recipes[0];

b82ca55fbe32b663b8740a91db53e9785afd5932
thompsons90 <sthompson082290@gmail.com>
2023-10-20 19:35:03 -0500

    Updated the playwright test file

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,30 +1,17 @@
+
 import { test, expect } from '@playwright/test';
-import { fetchMock } from 'fetch-mock';
+import axios from 'axios';
 
 test('RecipeComponent fetches data from API correctly', async ({ page }) => {
-  // Mock the network request
-  fetchMock.get('https://api.spoonacular.com/recipes/random?apiKey=a1ef51f91daf4b5baae9c503d96f2982', {
-    recipes: [
-      {
-        // mock response data
-        // Add your mock data here
-      },
-    ],
-  });
-
   // Launch the browser and open the test page
   await page.goto('http://localhost:5173/'); // Replace with your local server URL
 
   // Check if the API call is made correctly
-  const response = await fetch('https://api.spoonacular.com/recipes/random?apiKey=a1ef51f91daf4b5baae9c503d96f2982');
-  const data = await response.json();
+  const response = await axios.get('https://api.spoonacular.com/recipes/random?apiKey=a1ef51f91daf4b5baae9c503d96f2982'); // Replace with your actual API key
 
   // Check if the data fetched is not empty
-  const recipeData = data.recipes[0];
+  const recipeData = response.data.recipes[0];
   expect(recipeData).not.toBe(null);
 
   // You can perform more specific checks here, depending on your requirements
-
-  // Clean up the mock
-  fetchMock.restore();
 });
